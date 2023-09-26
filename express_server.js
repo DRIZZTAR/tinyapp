02fb9140e667b10f7dce099e6e23d313c57e4588
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-09-25 22:16:04 -0600

    Install and use cookie-parser to display username when logged in, also added logout button when user is logged on but currently is dead path

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser"); // Require cookie-parser
 
 app.set("view engine", "ejs");
+app.use(cookieParser()); // Use cookie-parser middleware
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,7 +26,10 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"] // Pass the username
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -42,11 +47,18 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"] // Pass the username
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"] // Pass the username
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -93,7 +105,6 @@ app.post("/login", (req, res) => {
   // Redirect the browser back to the /urls page
   res.redirect("/urls");
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
