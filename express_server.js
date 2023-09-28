584349ed26c63c7d47439b9ac9c265fc786a1488
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-09-28 16:35:07 -0600

    refactoring comments, and cleaning up spacing and syntax

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require("cookie-parser"); // Require cookie-parser
+const cookieParser = require("cookie-parser");
 
 app.set("view engine", "ejs");
 app.use(cookieParser()); // Use cookie-parser middleware
@@ -30,27 +30,23 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    user: users[req.cookies["user_id"]] // Pass the user object
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL; // Get the longURL from the form data
-  const shortURL = generateRandomString(); // Generate a random short URL
-
-  // Save the id-longURL pair to urlDatabase
-  urlDatabase[shortURL] = longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL; // Save the id-longURL pair to urlDatabase
 
   console.log(`Long URL: ${longURL}, Short URL: ${shortURL}`);
-
-  // Respond with a redirect to the new short URL page
   res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    user: users[req.cookies["user_id"]] // Pass the user object
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_new", templateVars);
 });
@@ -59,7 +55,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
-    user: users[req.cookies["user_id"]] // Pass the user object
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_show", templateVars);
 });
@@ -78,54 +74,38 @@ app.get("/u/:id", (req, res) => {
 
 app.post("/urls/:id/delete", (req, res) => {
   const shortURL = req.params.id; // Extract the shortURL from the request parameters
-
-  // Use the delete operator to remove the URL from urlDatabase
-  delete urlDatabase[shortURL];
-
-  // Redirect the client back to the urls_index page ("/urls")
+  delete urlDatabase[shortURL]; // Use the delete operator to remove the URL from urlDatabase
   res.redirect("/urls");
 });
 
-// Add a new route to handle URL updates
 app.post("/urls/:id/update", (req, res) => {
-  const shortURL = req.params.id; // Extract the shortURL from the request parameters
-  const newLongURL = req.body.newLongURL; // Get the updated longURL from the form data
-
-  // Update the long URL in the urlDatabase
-  urlDatabase[shortURL] = newLongURL;
-
-  // Redirect to the URL show page for the updated URL
-  res.redirect(`/urls/${shortURL}`);
+  const shortURL = req.params.id;
+  const newLongURL = req.body.newLongURL;
+  urlDatabase[shortURL] = newLongURL; // Update the long URL in the urlDatabase
+  res.redirect(`/urls/${shortURL}`); // Redirect to the URL show page for the updated URL
 });
 
 // Login
 app.post("/login", (req, res) => {
   const username = req.body.username; // Get the username from the request body
-
-  // Set a cookie named "user_id" with the value submitted in the request body
-  const userId = generateRandomString();
+  const userId = generateRandomString(); // Set a cookie named "user_id" with the value submitted in the request body
   res.cookie("user_id", userId);
 
   // Add the user object to the users object
   users[userId] = {
     id: userId,
     email: username,
-    password: "", // You can add password logic here
+    password: "",
   };
-
-  // Redirect the browser back to the /urls page
   res.redirect("/urls");
 });
 
 // Logout endpoint
 app.post("/logout", (req, res) => {
-  // Clear the user_id cookie
-  const userId = req.cookies["user_id"];
+  const userId = req.cookies["user_id"]; // Clear the user_id cookie
   delete users[userId];
   res.clearCookie("user_id");
-
-  // Redirect the user back to the /urls page (for now)
-  res.redirect("/urls");
+  res.redirect("/urls"); // Redirect the user back to the /urls page (for now)
 });
 
 // GET /register endpoint
