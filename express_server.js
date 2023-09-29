f2b5aef3432432553eb51fa9bec360df942c2280
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-09-29 11:37:24 -0600

    Changed logout endpoint to redirect to login page

--- express_server.js
+++ express_server.js
@@ -113,7 +113,7 @@ app.post("/logout", (req, res) => {
   const userId = req.cookies["user_id"]; // Clear the user_id cookie
   delete users[userId];
   res.clearCookie("user_id"); // Clear the user_id cookie
-  res.redirect("/urls"); // Redirect the user back to the /urls page (for now)
+  res.redirect("/login");
 });
 
 
